6f87b993b5b6349219068b3f036799d37b3c8cde
TonerowEEtim <anthony.eetim@gmail.com>
2022-10-21 12:28:25 -0700

    diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal line on the terminal using '\'
+ * @d: the number '\'needed
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < x; y++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
