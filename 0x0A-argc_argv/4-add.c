edfa66b3998a2d3d1e10aaabc58cd13230a2c54e
TonerowEEtim <anthony.eetim@gmail.com>
2022-11-04 10:38:44 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -27,3 +27,42 @@ int check_num(char *str)
 
 	return (1);
 }
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: count arguments
+ * @argv: arguments
+ * Return: Always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/*Declering variables*/
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+
+	while (count < argc) /*Go through the entire array*/
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]); /*ATOI --> convert str ti int*/
+			sum += str_to_int;
+		}
+
+	/*Condition if one number of the contain symbol that not digit*/
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	count++;
+	}
+
+	printf("%d\n", sum); /*Print sum*/
+
+	return (0);
+}
+
